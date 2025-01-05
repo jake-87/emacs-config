963fd5f82b06e999243073f0fad01103c91f1c08
Wren H <jhhenderson87@gmail.com>
2025-01-05 22:13:13 +1100

    new

---
+++ elpa/company-20240921.53/company-pkg.el
@@ -0,0 +1,12 @@
+(define-package "company" "20240921.53" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "852149619580dfeb5d37e435df40c22cfa851a34" :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
