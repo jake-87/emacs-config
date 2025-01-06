94728f403bfa6fc0d20c0e7f3802f7e27dc2134b
Wren H <jhhenderson87@gmail.com>
2025-01-06 13:52:47 +1100

    usual

--- init.el
+++ init.el
@@ -45,7 +45,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:height 170 :family "Fantasque Sans Mono"))))
+ '(org-document-title ((t (:height 2.0))))
+ '(org-level-1 ((t (:height 1.4))))
+ '(org-level-2 ((t (:height 1.2))))
+ '(org-level-3 ((t (:height 1.15))))
+ '(org-level-4 ((t (:height 1.1))))
+ '(org-level-5 ((t (:height 1.0)))))
 
 (set-fontset-font "fontset-default" 'unicode "JuliaMono")
 
